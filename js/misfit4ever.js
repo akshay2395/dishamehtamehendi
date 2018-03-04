3d007f9252fe1db167862d111336548635380e3f
akshay2395 <32189287+akshay2395@users.noreply.github.com>
2018-03-04 18:54:08 +0530

    Animation added in intro

--- js/misfit4ever.js
+++ js/misfit4ever.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+
+
 /* Smooth transition whike scrolling */
 
 
@@ -13,7 +15,27 @@ $(document).ready(function(){
     else if (scrollTop < 100) {
       $('#navigationBar').removeClass('scrolledNav');
     }
-});
+  });
+
+/*    fade in effect to intro text    */
+    $('#introText').animate({'opacity': '1'},2000);
+
+    $('.bgOne').scroll().delay(1000).animate({'opacity': '1'},1000);
+    $('.bgTwo').scroll().delay(1500).animate({'opacity': '1'},1000);
+    $('.bgThree').scroll().delay(2000).animate({'opacity': '1'},1000);
+
+    $("#Twitter").delay(1000).animate({'opacity': '1',
+                                       'font-size':'-=0.2em'}
+                                       ,500);
+
+    $("#facebook").delay(500).animate({'opacity': '1',
+                                       'font-size':'-=0.2em'}
+                                       ,500);
+
+    $("#instagram").delay(1500).animate({'opacity': '1',
+                                         'font-size':'-=0.2em'}
+                                         ,500);
+
 
 
 /*     Shadow effect on menu items      */
@@ -34,6 +56,15 @@ $(document).ready(function(){
   });
 
 
-  <!--adding margin to form elements-->
-
+  /*var lastScrollTop = 0;
+  $(window).scroll(function() {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      $('#introText').animate({top: '-=10px'},2);
+    }
+    else {
+      $('#introText').animate({top: '+=10px'},2);
+    }
+  });
+*/
 });
